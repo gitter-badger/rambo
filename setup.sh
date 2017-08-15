a12e7476a793f98fafcffe9579d15df04ae1407e
Michael Verhulst <michael@terminallabs.com>
2017-08-15 14:36:15 -0500

    first attempt at getting aws lightsail to work

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ cp auth/env_scripts/digitalocean.env.sh.dist digitalocean.env.sh
 cp auth/env_scripts/aws.env.sh.dist aws.env.sh
 
 vagrant plugin install vagrant-aws
+vagrant plugin install vagrant-lightsail
 vagrant plugin install vagrant-digitalocean
 vagrant plugin install vagrant-lxc
 vagrant plugin install vagrant-triggers
